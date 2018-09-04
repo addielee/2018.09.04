39394d37a88afe5b3ad55189d8a6c2a6655f772d
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-09-04 19:02:15 +0900

    Create 과제Electronics.java

---
+++ 과제Electronics.java
@@ -0,0 +1,15 @@
+class Electronics{
+	
+		//( Field, 생성자, setXxx(), getXxx())
+	   /**모델번호(modelNo) - int,
+       모델이름(modelName) - String
+       가격(modelPrice) - int , 
+       설명(modelDetail) - String)*/
+
+	   private int modelNo;
+	   private String modelName;
+	   private int modelPrice;
+	   private String modelDetail;
+
+
+}
