befedba73b6cdb10738c73cc7b1522febbd3cb67
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-09-04 19:02:36 +0900

    Create 과제ElectronicsService.java

---
+++ 과제ElectronicsService.java
@@ -0,0 +1,10 @@
+class ElectronicsService{
+	
+		/**
+		전역변수 : Electronics electronics [] = new Electronics[5];
+          - public void save(String [][] data){}
+          - public void printAll(){}
+          - public void searchBymodelNo(int modelNo){}
+		  */
+	
+}
