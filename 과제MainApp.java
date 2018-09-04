0160b8195351ee446e69d0496ed91e1740fd40dd
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-09-04 19:02:58 +0900

    Create 과제MainApp.java

---
+++ 과제MainApp.java
@@ -0,0 +1,15 @@
+class MainApp{
+	public static void main(String[] args) {
+		/**
+		메인메소드 작성
+           - 초기치 데이터 2번항목이 메인메소드에 있다.
+           - ElectronicsService의 save를 호출한다.
+           - ElectronicsService의 printAll() 호출한다.
+           - ElectronicsService의 searchBymodelNo(int modelNo) 호출한다.
+             (modelNo는 실행할때 인수(args)로 전달된 정보를 준다)
+		*/
+
+		
+		
+	}
+}
