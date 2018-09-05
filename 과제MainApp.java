74c8aae8482eb46ae64de17f27fb1331fc173c24
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-09-05 14:06:48 +0900

    Update 과제MainApp.java

--- 과제MainApp.java
+++ 과제MainApp.java
@@ -1,15 +1,24 @@
-class MainApp{
+class MainApp {
 	public static void main(String[] args) {
-		/**
-		메인메소드 작성
-           - 초기치 데이터 2번항목이 메인메소드에 있다.
-           - ElectronicsService의 save를 호출한다.
-           - ElectronicsService의 printAll() 호출한다.
-           - ElectronicsService의 searchBymodelNo(int modelNo) 호출한다.
-             (modelNo는 실행할때 인수(args)로 전달된 정보를 준다)
-		*/
+     String data [][] = {
+		  {"100" , "에어컨" , "250000", "삼성 무풍에어컨"} ,
+		  {"200" , "세탁기" , "350000", "LG 세탁짱"} ,
+		  {"300" , "냉장고" , "500000", "냉장고입니다."} ,
+		  {"400" , "전자렌지" , "640000", "전자렌즈입니다."} ,
+		  {"500" , "밥통" , "450000", "쿠쿠밥솥 최고!"} , 
+     };
 
-		
-		
+        ElectronicsService service = new ElectronicsService();
+    
+          // - ElectronicsService의 save를 호출한다.
+		  service.save(data);
+          // - ElectronicsService의 printAll() 호출한다.
+		  service.printAll();
+
+          // - ElectronicsService의 searchBymodelNo(int modelNo) 호출한다.
+          //   (modelNo는 실행할때 인수(args)로 전달된 정보를 준다)
+          if(args.length > 0 )
+		      service.searchBymodelNo(Integer.parseInt(args[0]));
+
 	}
 }
