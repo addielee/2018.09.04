d59c76cb896302661b3a4c0ff3d9dcf1201533e4
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-09-05 14:00:49 +0900

    Update 과제Electronics.java

--- 과제Electronics.java
+++ 과제Electronics.java
@@ -1,15 +1,24 @@
 class Electronics{
-	
-		//( Field, 생성자, setXxx(), getXxx())
-	   /**모델번호(modelNo) - int,
-       모델이름(modelName) - String
-       가격(modelPrice) - int , 
-       설명(modelDetail) - String)*/
+	   // Field, 생성자, setXxx(), getXxx()
 
 	   private int modelNo;
 	   private String modelName;
 	   private int modelPrice;
 	   private String modelDetail;
 
+	   Electronics(){
+	   }
+	   Electronics(int modelNo,String modelName){
+		   this.modelNo=modelNo;
+		   this.modelName=modelName;
+	   }
+	   Electronics(int modelNo, String modelName, int modelPrice, String modelDetail){
+		   this(modelNo,modelName); //생성자 호출
+		   this.modelPrice=modelPrice;
+		   this.modelDetail=modelDetail;
+	   }
 
+	  
+	  public void setModelNo(int modelNo){
+	  }
 }
